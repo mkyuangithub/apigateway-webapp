703fa8ebe6d8e158621aaf0d2078bc57c5afcb3b
mk.yuan <mk.yuan@datsch.com.cn>
2025-07-18 16:17:12 +0800

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,68 @@
+import { defineConfig, loadEnv } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+import path from "path";
+
+const consoleEnv = (env) => {
+	console.log("")
+	Object.keys(env).map(k => {
+		console.log("\x1B[31m%s\x1B[0m", "全局配置 " + k + " = " + env[k]);
+	});
+	console.log("")
+}
+
+export default defineConfig(conf => {
+	const env = loadEnv(conf.mode, process.cwd(), 'P_');
+	consoleEnv(env);
+	return {
+		plugins: [vue()],
+		resolve: {
+			alias: {
+				"@": path.resolve(__dirname, "src"),
+				"vue-i18n": "vue-i18n/dist/vue-i18n.cjs.js"
+			}
+		},
+		build: {
+			outDir: "release",
+			minify: "terser",
+			chunkSizeWarningLimit: 1000,
+			terserOptions: {
+				compress: {
+					drop_console: true,
+					drop_debugger: true
+				}
+			},
+			rollupOptions: {
+				input: {
+					main: "./index.html"
+				},
+				output: {
+					manualChunks(id) {
+						if (id.includes('node_modules')) {
+							return id
+								.toString()
+								.split('node_modules/')[1]
+								.split('/')[0]
+								.toString();
+						}
+					}
+				},
+			}
+		},
+		define: {
+			"process.env": env
+		},
+		server: {
+			host: 'localhost',
+			port: 8200,
+			proxy: {
+				"/venus": {
+					target: "http://localhost:9080",
+					changeOrigin: true,
+					ws: true,
+					rewrite: path => path.replace("/venus", ""),
+				}
+			}
+		}
+	};
+})
